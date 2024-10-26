8bfe2614587283ebb7e16bb4d15915a2b2e07137
Michal Hercík <hercik77@seznam.cz>
2024-10-26 15:49:13 +0200

    init router setup

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func Logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r)
+		log.Println(r.Method, r.URL.Path)
+	})
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+
+	router := http.NewServeMux()
+
+	//////////////////////////////////////////
+	// Handlers
+	//////////////////////////////////////////
+	// eg.: router.HandleFunc("GET /", handleGetHello)
+
+	//////////////////////////////////////////
+	// Server setup
+	//////////////////////////////////////////
+	server := http.Server{
+		Addr:    ":8000",
+		Handler: Logging(router),
+	}
+
+	log.Println("Server starting ...")
+	log.Println("http://localhost:8000/")
+
+	server.ListenAndServe()
 }
