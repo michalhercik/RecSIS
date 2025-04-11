8202b559dd26508235f0de081512c58a9577ff21
Michal <mike.medek@gmail.com>
2025-04-11 19:15:53 +0200

    coursedeatil -> new design version, to be continued

--- src/coursedetail/translations.go
+++ src/coursedetail/translations.go
@@ -39,9 +39,11 @@ type text struct {
 	NotRated         string
 	NoRatings        string
 	SISLink          string
+	Description      string
+	NoDescription    string
+	Survey           string
+	NoSurvey         string
 	Detail           string
-	Comments         string
-	NoComments       string
 	CourseWithCode   string
 	NotFound         string
 	// utils
@@ -90,9 +92,11 @@ var texts = map[language.Language]text{
 		NotRated:         "Nehodnoceno",
 		NoRatings:        "Žádná hodnocení",
 		SISLink:          "Odkaz do SIS",
+		Description:      "Popis",
+		NoDescription:    "Pro tento předmět není k dispozici žádný popis.",
+		Survey:           "Anketa",
+		NoSurvey:         "Pro tento předmět nejsou žádné komentáře.",
 		Detail:           "Detailní informace",
-		Comments:         "Komentáře",
-		NoComments:       "Pro tento předmět nejsou žádné komentáře.",
 		CourseWithCode:   "Předmět s kódem ",
 		NotFound:         " nenalezen.",
 		// utils
@@ -117,8 +121,8 @@ var texts = map[language.Language]text{
 		StateOfCourse:    "State of the course",
 		LanguageOfCourse: "Language",
 		AdditionalInfo:   "Additional information",
-		Prerequisites:    "Pre-requisite",
-		Corequisites:     "Co-requisite",
+		Prerequisites:    "Pre-requisites",
+		Corequisites:     "Co-requisites",
 		Interchange:      "Interchangeability",
 		Incompatible:     "Incompatibility",
 		Classes:          "Class(es)",
@@ -130,9 +134,11 @@ var texts = map[language.Language]text{
 		NotRated:         "Not rated",
 		NoRatings:        "No ratings",
 		SISLink:          "Link to SIS",
+		Description:      "Description",
+		NoDescription:    "No description available for this course.",
+		Survey:           "Survey",
+		NoSurvey:         "No surveys for this course.",
 		Detail:           "Detailed information",
-		Comments:         "Comments",
-		NoComments:       "No comments for this course.",
 		CourseWithCode:   "Course with code ",
 		NotFound:         " not found.",
 		// utils
--- src/utils/translations.go
+++ src/utils/translations.go
@@ -1,36 +1,38 @@
 package utils
 
+import "github.com/michalhercik/RecSIS/language"
+
 type Text struct {
-	Language string
-	Home string
-	Courses string
-	Blueprint string
-	DegreePlan string 
-	Login string
-	Contact string
+	Language   language.Language
+	Home       string
+	Courses    string
+	Blueprint  string
+	DegreePlan string
+	Login      string
+	Contact    string
 }
 
 func (t Text) LangLink(URL string) string {
-	return "/" + t.Language + URL
+	return "/" + string(t.Language) + URL
 }
 
 var Texts = map[string]Text{
 	"cs": {
-		Language: "cs",
-		Home: "Domů",
-		Courses: "Hledání",
-		Blueprint: "Blueprint",
+		Language:   language.CS,
+		Home:       "Domů",
+		Courses:    "Hledání",
+		Blueprint:  "Blueprint",
 		DegreePlan: "Studijní plán",
-		Login: "Přihlášení",
-		Contact: "V případě jakýchkoliv problémů kontaktujte tým RecSIS.",
+		Login:      "Přihlášení",
+		Contact:    "V případě jakýchkoliv problémů kontaktujte tým RecSIS.",
 	},
 	"en": {
-		Language: "en",
-		Home: "Home",
-		Courses: "Search",
-		Blueprint: "Blueprint",
+		Language:   language.EN,
+		Home:       "Home",
+		Courses:    "Search",
+		Blueprint:  "Blueprint",
 		DegreePlan: "Degree plan",
-		Login: "Login",
-		Contact: "In case of any problems, please contact the RecSIS team.",
+		Login:      "Login",
+		Contact:    "In case of any problems, please contact the RecSIS team.",
 	},
 }
