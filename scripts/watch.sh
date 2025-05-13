d1574c5f58a4c74682ce34dd81038e2cc6bbf2ee
Michal Hercík <hercik77@seznam.cz>
2025-05-13 16:51:11 +0200

    fix

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 (templ generate; go build -C ./mock_cas -o mockcas.exe; ./mock_cas/mockcas.exe --cert server.crt --key server.key) &
-wgo -file=".go" -file=".templ" -xfile="_templ.go" templ generate :: go build -o recsis :: ./recsis
+wgo -file=".go" -file=".templ" -xfile="_templ.go" templ generate :: go build -C ./src -o recsis :: ./src/recsis
