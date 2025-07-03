25050c6d9e6f8434fb4f750441b6e22ea4be06b2
Michal Hercík <hercik77@seznam.cz>
2025-07-03 18:37:18 +0200

    dockerize etl

--- etl/docker-cmd.sh
+++ etl/docker-cmd.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-# sshpass -p "$SSH_PASS" ssh -T -L 10502:mffout.karlov.mff.cuni.cz:10502 hercik@acheron.ms.mff.cuni.cz -p 42049 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null &
-
 chmod 600 /root/.ssh/id_rsa
 
-autossh -f -M 0 -gNC -o "ExitOnForwardFailure=yes" -o "ServerAliveInterval=10" -o "ServerAliveCountMax=3" -L 10502:mffout.karlov.mff.cuni.cz:10502 hercik@acheron.ms.mff.cuni.cz -p 42049 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
+autossh -f -M 0 -gNC \
+-o "ExitOnForwardFailure=yes" \
+-o "ServerAliveInterval=10" \
+-o "ServerAliveCountMax=3" \
+-L 10502:mffout.karlov.mff.cuni.cz:10502 $ACHERON_USER@acheron.ms.mff.cuni.cz \
+-p 42049 \
+-o StrictHostKeyChecking=no \
+-o UserKnownHostsFile=/dev/null
+
 timeout=30
 while ! nc -z localhost 10502; do
     sleep 1
@@ -15,6 +21,6 @@ while ! nc -z localhost 10502; do
     fi
 done
 
-/etl
+/etl --config /app/config.docker.toml
 
 pkill autossh
