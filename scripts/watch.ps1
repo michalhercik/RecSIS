03afef99f1e318d14f70b10ba44b4d42eb80c969
Michal Hercík <hercik77@seznam.cz>
2025-07-16 19:16:42 +0200

    dockerize mockcas

--- scripts/watch.ps1
+++ scripts/watch.ps1
@@ -1,24 +1,7 @@
-try {
-    $jobs = @(
-        Start-ThreadJob {
-            templ generate -path ./mock_cas
-            go build -C ./mock_cas -o mockcas.exe
-            ./mock_cas/mockcas.exe --cert server.crt --key server.key
-        } -Name 'SomeJob'
-
-        Start-ThreadJob {
-            wgo `
-                -file='.go' `
-                -file='.templ' `
-                -xfile='_templ.go' `
-                templ generate -path ./webapp :: `
-                go build -C ./webapp -o RecSIS.exe :: `
-                ./webapp/RecSIS.exe --config ./webapp/config.dev.toml
-        } -Name 'OtherJob'
-    )
-
-    $jobs | Receive-Job -Wait -AutoRemoveJob
-}
-finally {
- $jobs | Remove-Job -Force
-}
+wgo `
+    -file='.go' `
+    -file='.templ' `
+    -xfile='_templ.go' `
+    templ generate -path ./webapp :: `
+    go build -C ./webapp -o RecSIS.exe :: `
+    ./webapp/RecSIS.exe --config ./webapp/config.dev.toml
--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-(templ generate; go build -C ./mock_cas -o mockcas.exe; ./mock_cas/mockcas.exe --cert server.crt --key server.key) &
-wgo -file=".go" -file=".templ" -xfile="_templ.go" templ generate :: go build -C ./webapp -o recsis :: ./webapp/recsis --config ./webapp/config.dev.toml
+wgo \
+    -file=".go" -file=".templ" \
+    -xfile="_templ.go" \
+    templ generate :: \
+    go build -C ./webapp -o recsis :: \
+    ./webapp/recsis --config ./webapp/config.dev.toml
