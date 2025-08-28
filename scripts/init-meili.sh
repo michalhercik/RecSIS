9871da1143003b282bc236067a1b2b0d989104aa
Michal Hercík <hercik77@seznam.cz>
2025-08-28 21:36:12 +0200

    add annotation to embedding in bash script

--- scripts/init-meili.sh
+++ scripts/init-meili.sh
@@ -42,7 +42,7 @@ response=$(curl -s -o /dev/null -w "%{http_code}" -X PATCH \
         "response": {
             "embedding": "{{embedding}}"
         },
-        "documentTemplate": "University course with title {{doc.title.en}}"
+        "documentTemplate": "University course with title {{doc.title.en}} {% if doc.annotation != nil %} {{doc.annotation[0]}} {% endif %}"
     }
   }' \
   "$BASE_URL/indexes/courses/settings/embedders")
