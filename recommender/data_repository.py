8715a5e9466ffb794eb27b6e032ee3a1e24adc1e
Michal Hercík <hercik77@seznam.cz>
2025-12-07 16:02:30 +0100

    add pamela, obor, preq, searchable_povinn to DataRepository

--- recommender/data_repository.py
+++ recommender/data_repository.py
@@ -1,16 +1,24 @@
 import os
+
 import pandas as pd
 from sqlalchemy import create_engine
+
 
 class DataRepository:
     def __init__(self):
         host = os.environ.get("POSTGRES_HOST", "localhost")
-        self.__engine = create_engine(f"postgresql://recommender:recommender@{host}:5432/recsis")
+        self.__engine = create_engine(
+            f"postgresql://recommender:recommender@{host}:5432/recsis"
+        )
         self.povinn = self.__fetch_table("povinn")
         self.zkous = self.__fetch_table("zkous")
         self.studium = self.__fetch_table("studium")
         self.stud_plan = self.__fetch_table("stud_plan")
-    
+        self.obor = self.__fetch_table("obor")
+        self.pamela = self.__fetch_table("pamela")
+        self.preq = self.__fetch_table("preq")
+        self.searchable_povinn = self.__fetch_table("searchable_povinn")
+
     def sql_query(self, query: str):
         with self.__engine.connect() as conn:
             result = pd.read_sql_query(query, conn)
